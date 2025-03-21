3fedae5f202575d3c44838803419f55809333875
Khanhlam1186 <khanhyt2002@gmail.com>
2025-03-21 10:19:06 +0700

    update

--- src/components/Pages/Layout/Footer.js
+++ src/components/Pages/Layout/Footer.js
@@ -3,81 +3,74 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 const Footer = () => {
   return (
     <>
-      <footer className="ftco-footer ftco-no-pb ftco-section">
-        <div className="container">
-          <div className="row mb-5">
-            <div className="col-md-6 col-lg-4 d-flex flex-column align-items-between">
-              <div className="ftco-footer-widget mb-4 text-between">
-                <h2 className="ftco-heading-2">Taste.it</h2>
-                <ul className="ftco-footer-social list-unstyled mt-3 d-flex justify-content-center">
-                <li className="mr-3">
+  <footer className="ftco-footer ftco-no-pb ftco-section">
+    <div className="container">
+      <div className="row mb-5">
+        {/* Cột logo & mạng xã hội */}
+        <div className="col-md-6 col-lg-3 d-flex flex-column align-items-between">
+          <div className="ftco-footer-widget mb-4 text-between">
+            <h2 className="ftco-heading-2">Taste.it</h2>
+            <ul className="ftco-footer-social list-unstyled mt-3 d-flex justify-content-center">
+              <li className="mr-3">
                 <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-                <span className="fa fa-instagram" />
+                  <span className="fa fa-instagram" />
                 </a>
-                </li>                                     
-                <li className="mr-3">
+              </li>                                     
+              <li className="mr-3">
                 <a href="https://www.facebook.com/profile.php?id=61573711116533" target="_blank" rel="noopener noreferrer">
-                <span className="fa fa-facebook" />
+                  <span className="fa fa-facebook" />
                 </a>
-                </li>
-                <li className="mr-3">
+              </li>
+              <li className="mr-3">
                 <a href="https://www.youtube.com/@safedu-antoanviet" target="_blank" rel="noopener noreferrer">
-                <span className="fa fa-youtube" />
+                  <span className="fa fa-youtube" />
                 </a>
-                </li>
-                </ul>
-              </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Cột hình ảnh */}
+        <div className="col-md-6 col-lg-3">
+          <div className="ftco-footer-widget mb-4">
+            <h2 className="ftco-heading-2">Hình Ảnh</h2>
+            <div className="thumb d-sm-flex">
+              <a href="#" className="thumb-menu img" style={{ backgroundImage: "url(images/skill-1.jpg)" }}></a>
+              <a href="#" className="thumb-menu img" style={{ backgroundImage: "url(images/chef-4.jpg)" }}></a>
+              <a href="#" className="thumb-menu img" style={{ backgroundImage: "url(images/skill-1.jpg)" }}></a>
             </div>
-            
-            <div className="col-md-6 col-lg-4">
-              <div className="ftco-footer-widget mb-4">
-                <h2 className="ftco-heading-2">Hình Ảnh</h2>
-                <div className="thumb d-sm-flex">
-                  <a
-                    href="#"
-                    className="thumb-menu img"
-                    style={{ backgroundImage: "url(images/skill-1.jpg)" }}
-                  ></a>
-                  <a
-                    href="#"
-                    className="thumb-menu img"
-                    style={{ backgroundImage: "url(images/chef-4.jpg)" }}
-                  ></a>
-                  <a
-                    href="#"
-                    className="thumb-menu img"
-                    style={{ backgroundImage: "url(images/skill-1.jpg)" }}
-                  ></a>
-                </div>
-                <div className="thumb d-flex">
-                  <a
-                    href="#"
-                    className="thumb-menu img"
-                    style={{ backgroundImage: "url(images/chef-4.jpg)" }}
-                  ></a>
-                  <a
-                    href="#"
-                    className="thumb-menu img"
-                    style={{ backgroundImage: "url(images/skill-1.jpg)" }}
-                  ></a>
-                  <a
-                    href="#"
-                    className="thumb-menu img"
-                    style={{ backgroundImage: "url(images/chef-4.jpg)" }}
-                  ></a>
-                </div>
-              </div>
+            <div className="thumb d-flex">
+              <a href="#" className="thumb-menu img" style={{ backgroundImage: "url(images/chef-4.jpg)" }}></a>
+              <a href="#" className="thumb-menu img" style={{ backgroundImage: "url(images/skill-1.jpg)" }}></a>
+              <a href="#" className="thumb-menu img" style={{ backgroundImage: "url(images/chef-4.jpg)" }}></a>
             </div>
           </div>
         </div>
-        <div className="container-fluid px-0 bg-primary py-3">
-          <div className="row no-gutters">
-            <div className="col-md-12 text-center">
-              <p className="mb-0">SAFEDU VN</p>
-            </div>
+
+        {/* Cột thông tin liên hệ */}
+        <div className="col-md-6 col-lg-3">
+          <div className="ftco-footer-widget mb-4">
+            <h2 className="ftco-heading-2">Liên Hệ</h2>
+            <ul className="list-unstyled">
+              <li><span className="fa fa-envelope"></span> Email: vp.vienkhgdat@gmail.com</li>
+              <li><span className="fa fa-phone"></span> Số điện thoại: 098 522 1159</li>
+              <li><span className="fa fa-globe"></span> Website: viengiaoducantoan.edu.vn</li>
+              <li><span className="fa fa-map-marker"></span>A2 D6, Ngõ 5, Thọ Tháp, Dịch Vọng, Cầu Giấy, Hà Nội </li>
+            </ul>
           </div>
         </div>
-      </footer>
+      </div>
+    </div>
+
+    {/* Dòng chữ bản quyền */}
+    <div className="container-fluid px-0 bg-primary py-3">
+      <div className="row no-gutters">
+        <div className="col-md-12 text-center">
+          <p className="mb-0">SAFEDU VN</p>
+        </div>
+      </div>
+    </div>
+  </footer>
     </>
   );
 };
--- src/components/Pages/Layout/Header.js
+++ src/components/Pages/Layout/Header.js
@@ -37,83 +37,76 @@ const Header = () => {
     <>
       
       <nav
-        className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light"
-        id="ftco-navbar"
-      >
-        <div className="container">
-          <Link to="/" className="navbar-brand">
-            <img
-              src="images/blacklogo.png"
-              alt="Logo"
-              style={{
-                height: "auto",
-                width: "100px",
-              }}
-            />
-          </Link>
+  className="navbar navbar-expand-lg navbar-light bg-white ftco-navbar-light"
+  id="ftco-navbar"
+>
+  <div className="container">
+    <Link to="/" className="navbar-brand">
+      <img
+        src="images/blacklogo.png"
+        alt="Logo"
+        style={{
+          height: "auto",
+          width: "100px",
+        }}
+      />
+    </Link>
 
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#ftco-nav"
-            aria-controls="ftco-nav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="oi oi-menu" /> Menu
-          </button>
-          <div  className="collapse navbar-collapse" id="ftco-nav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-                <Link to="/" className="nav-link">
-                <i className="fa-solid fa-house fa-2x"></i>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/" className="nav-link">
-                  GIỚI THIỆU
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/" className="nav-link">
-                  KHÓA HỌC
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/" className="nav-link">
-                  TIN TỨC
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/" className="nav-link">
-                  LIÊN HỆ
-                </Link>
-              </li>
-              <li className="nav-item">
-                {userName ? (
-                  <Link to="/customer/profile" className="nav-link">
-                    Xin chào, {userName}
-                  </Link>
-                ) : (
-                  <Link to="/authentication" className="nav-link">
-                    GIÁO ÁN 
-                  </Link>
-                )}
-              </li>
-              {/* <li className="nav-item">
-                <Link to="/menu" className="nav-link" onClick={openCartModal}>
-                  <i className="fa-solid fa-cart-shopping fa-2x">
-                    {totalItems > 0 && (
-                      <span className="badge badge-danger">{totalItems}</span>
-                    )}
-                  </i>
-                </Link>
-              </li> */}
-            </ul>
-          </div>
-        </div>
-      </nav>
+    {/* Nút Menu - Đã sửa */}
+    <button
+  className="navbar-toggler"
+  type="button"
+  data-bs-toggle="collapse"  /* Thay data-toggle thành data-bs-toggle */
+  data-bs-target="#ftco-nav"
+  aria-controls="ftco-nav"
+  aria-expanded="false"
+  aria-label="Toggle navigation"
+>
+  <i className="fa-solid fa-bars"></i> {/* Icon 3 gạch */}
+</button>
+
+    <div className="collapse navbar-collapse" id="ftco-nav">
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item active">
+          <Link to="/" className="nav-link">
+            <i className="fa-solid fa-house fa-2x"></i>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            GIỚI THIỆU
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            KHÓA HỌC
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            TIN TỨC
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            LIÊN HỆ
+          </Link>
+        </li>
+        <li className="nav-item">
+          {userName ? (
+            <Link to="/customer/profile" className="nav-link">
+              Xin chào, {userName}
+            </Link>
+          ) : (
+            <Link to="/authentication" className="nav-link">
+              GIÁO ÁN
+            </Link>
+          )}
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
       <Modal
         title="GIỎ HÀNG"
         visible={isCartModalOpen}
